a7fdb251ae4e75793f4fcf539b35b00f22f4ad66
phamdung2209 <phamdung.22092003@gmail.com>
2023-12-11 02:20:28 +0700

    Build the ui header part #1

--- src/components/Layout/DefaultLayout/components/Header/index.js
+++ src/components/Layout/DefaultLayout/components/Header/index.js
@@ -1,7 +1,10 @@
 import classNames from 'classnames/bind'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './Header.module.scss'
+import images from '~/assets/images'
 
 const cx = classNames.bind(styles)
 
@@ -9,8 +12,24 @@ function Header() {
     return (
         <header className={cx('header')}>
             <div className={cx('inner')}>
-                {/* logo */}
-                {/* search */}
+                <Link to='/'>
+                    <img src={images.logo} alt='tiktok' />
+                </Link>
+
+                <div className={cx('search')}>
+                    <input type='text' placeholder='Search' spellCheck={false} />
+                    <button className={cx('close')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('action')}>
+
+                </div>
             </div>
         </header>
     )
