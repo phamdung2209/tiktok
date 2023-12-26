7807f056b086730d7ba98b18699e86c9102b5de9
phamdung2209 <phamdung.22092003@gmail.com>
2023-12-26 22:54:01 +0700

    Build ui video details #1

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -304,7 +304,7 @@ export const Report = ({ className }) => (
 )
 
 export const ReportMode = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" viewBox="0 0 48 48" height="15" fill="currentColor" class="css-159e23g-StyledIconFlag e11s2kul19">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" viewBox="0 0 48 48" height="15" fill="currentColor">
         <path d="M9 9.31v17.75c5.27-1.73 11.45-2.05 16.16 1.31 4.03 2.88 9.52 2.01 13.84.32V10.94c-5.27 1.73-11.45 2.05-16.16-1.31-4-2.86-9.53-2.01-13.84-.32ZM43 8v22a2 2 0 0 1-1.1 1.79c-5.83 2.9-13.5 3.82-19.06-.16C18.8 28.75 13.32 29.6 9 31.3V44a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V8c0-.75.44-1.45 1.1-1.79 5.75-2.87 13.62-3.73 19.06.16 4.37 3.13 10.43 2.04 14.95-.16C41.4 5.56 43 6.54 43 8Z"></path>
     </svg>
 )
--- src/pages/VideoDetail/VideoDetail.js
+++ src/pages/VideoDetail/VideoDetail.js
@@ -5,6 +5,8 @@ import styles from './VideoDetail.module.scss'
 import Button from "~/components/Button"
 import { CloseIconBold, ReportMode } from '~/assets/icons'
 import Search from '~/layouts/DefaultLayout/components/Search'
+import Image from "~/components/Image"
+import { Link } from "react-router-dom"
 
 const cx = classNames.bind(styles)
 
@@ -29,7 +31,7 @@ function VideoDetail() {
 
                 <div className={cx('video__content')}>
                     <ReactPlayer
-
+                        url='https://files.fullstack.edu.vn/f8-tiktok/videos/8-630268a08faa9.mp4'
                     >
                         x
                     </ReactPlayer>
@@ -41,7 +43,56 @@ function VideoDetail() {
             </div>
 
             <div className={cx('description')}>
+                <div className={cx('des-header')}>
+                    <div className={cx('header-container')}>
+                        <div className={cx('profile-wrapper')}>
+                            <div className={cx('pro-des')}>
+                                <div className={cx('pro-des-info')}>
+                                    <Link
+                                        to='/@sondnf8pro'
+                                    // className={cx('pro-des-info__link')}
+                                    >
+                                        <Image
+                                            src='https://files.fullstack.edu.vn/f8-tiktok/users/11/630266fd71515.jpg'
+                                            alt=' '
+                                            className={cx('pro-des-info__avatar')}
+                                        />
 
+                                        <div className={cx('info-user')}>
+                                            <div className={cx('pro-des-info__nickname')}>
+                                                <span>sondnf8pro</span>
+                                            </div>
+
+                                            <div className={cx('pro-des-info__name')}>
+                                                <span>Sơn Đặng</span>
+                                                <span style={{ margin: '0px 4px' }}> · </span>
+                                                <span>10-13</span>
+                                            </div>
+                                        </div>
+                                    </Link>
+
+                                    <Button primary>
+                                        Follow
+                                    </Button>
+                                </div>
+
+                                <div className={cx('pro-des-content')}>
+                                    <div className={cx('main-content')}>
+                                        Tóm tắt chuyện tình Việt - Nhật của tụi mình #couple #lovestory
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={cx('tab-menu-wrapper')}>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className={cx('des-bottom')}>
+
+                </div>
             </div>
         </div>
     )
